997ce66bcf97d52178a9458d1c1b654cac8cc86e
EC2 Default User <y.kor4562xoxo@gmail.com>
2023-04-23 04:19:56 +0000

    [add]0423

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,58 @@
+class BooksController < ApplicationController
+  #def new
+  #end不要かも？
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book)
+    else
+      render :index
+    end
+  end 
+  
+  def index
+    #@user = User.find(params[:id])
+    @book = Book.new
+    @books = Book.all
+  end
+  
+  def show
+    @book = Book.find(params[:id])
+    @user = current_user
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+  end
+end
---
+++ test/controllers/uses_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class UsesControllerTest < ActionDispatch::IntegrationTest
+  test "should get show" do
+    get uses_show_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get uses_index_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get uses_edit_url
+    assert_response :success
+  end
+end
